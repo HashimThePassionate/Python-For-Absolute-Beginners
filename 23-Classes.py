d46e3ceab3a7d0c3f86c1081aa03dccab1f1de04
Muhammad Hashim <hashiimtahir@gmail.com>
2024-02-22 09:35:38 +0500

    Making Custom Containers

--- 23-Classes.py
+++ 23-Classes.py
@@ -596,7 +596,47 @@ print(p1 == p2)
 print(p3 > p4)
 print(combine)
 
+# Making Custom Containers
 
+
+class TagCloud:
+    def __init__(self) -> None:
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0)+1
+
+    def __getitem__(self, tag):
+        return self.tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __iter__(self):
+        return iter(self.tags)
+
+
+# cloud = TagCloud()
+# cloud.add('python')
+# cloud.add('python')
+# cloud.add('python')
+# cloud.add('python')
+# print(cloud.tags)
+tag_cloud = TagCloud()
+tag_cloud.add("Python")
+tag_cloud.add("python")
+tag_cloud.add("Java")
+tag_cloud.add("python")
+tag_cloud.add("C++")
+print(tag_cloud["python"])  # Output: 3
+tag_cloud["java"] = 2
+print(tag_cloud["java"])  # Output: 2
+print(len(tag_cloud))  # Output: 3
+for tag in tag_cloud:
+    print(tag, tag_cloud[tag])
 # Single inheritance
 '''
 A class can reuse another class by inheriting it. When a child class 
