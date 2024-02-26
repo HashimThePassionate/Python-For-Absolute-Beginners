fd51fdc277ca1b1c48f60bc2d8d7c26e1efa202a
Muhammad Hashim <hashiimtahir@gmail.com>
2024-02-26 18:18:25 +0500

    Polymorphism

--- 23-Classes.py
+++ 23-Classes.py
@@ -925,6 +925,43 @@ print(parrot.speak())  # Output: Polly says Hello!
 print(parrot.chirp())  # Output: Polly chirps loudly!
 
 # --------------------------------------------------------------
+# PolyMorphism
+from typing import Union, overload
+
+class Adder:
+    @overload
+    def add(self, x: int, y: int) -> int:
+        ...
+        
+    @overload
+    def add(self, x: float, y: float) -> float:
+        ...
+        
+    @overload
+    def add(self, x: str, y: str) -> str:
+        ...
+        
+    
+    def add(self, x: Union[int, float, str], y: Union[int, float, str]) -> Union[int, float, str]:
+        if isinstance(x, int) and isinstance(y, int):
+            return x + y
+        elif isinstance(x, float) and isinstance(y, float):
+            return x + y
+        elif isinstance(x, str) and isinstance(y, str):
+            return x + y
+        else:
+            raise TypeError("Invalid argument types!")
+
+# Usage examples
+adder = Adder()
+result1 = adder.add(1, 2)  # Should return 3
+result2 = adder.add(1.5, 2.5)  # Should return 4.0
+result3 = adder.add("Hello, ", "world!")  # Should return "Hello, world!"
+# res4 = adder.add(1,'mystring')
+print(result1)
+print(result2)
+print(result3)
+# print(res4)
 
 
 class Calculation1:
